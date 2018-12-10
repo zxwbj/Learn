03576a987b472bc1e2c5233ff87630a1eb3bd2c5
zxw <1019364135@qq.com>
2018-12-10 18:12:41 +0800

    2018.12.10: example: random walk

--- DataAnalysis/numpy/random_walk.py
+++ DataAnalysis/numpy/random_walk.py
@@ -20,29 +20,31 @@ nsteps = 1000
 draws = np.random.randint(0, 2, size=nsteps)
 steps = np.where(draws > 0, 1, -1)
 walk = steps.cumsum()
+print(steps)
 print(walk)
 print(walk.min())
 print(walk.max())
+print(np.abs(walk) >= 10)
 print((np.abs(walk) >= 10).argmax())
 print('-----------------------------------------')
 nwalks = 5000
 nsteps = 1000
 draws = np.random.randint(0, 2, size=(nwalks, nsteps))
 steps = np.where(draws > 0, 1, -1)
-walks = steps.cumsum(1)
+walks = steps.cumsum(axis=1)
 print(walks.max())
 print(walks.min())
 
-hits30 = (np.abs(walks) >= 30).any(1)
-print(hits30)
+hits30 = (np.abs(walks) >= 30).any(axis=1)
+print(hits30.shape)
 print(hits30.sum())
-
 print(hits30)
 print(walks)
-print(walks[hits30])
 
-crossing_times = (np.abs(walks[hits30]) >= 30).argmax(1)
+print((walks[hits30]))                  # boolean indexing
+print((walks[hits30]).shape)
+
+crossing_times = (np.abs(walks[hits30]) >= 30).argmax(axis=1)
 print(crossing_times)
 print(crossing_times.mean())
-
 
