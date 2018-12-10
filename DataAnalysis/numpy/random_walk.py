87e8de60240b9beca0c4d40e661b910e868ca34b
zxw <1019364135@qq.com>
2018-12-10 16:45:37 +0800

    2018.12.10: learn numpy

---
+++ DataAnalysis/numpy/random_walk.py
@@ -0,0 +1,48 @@
+import random
+import matplotlib.pyplot as plt
+
+position = 0
+walk = [position]
+steps = 1000
+for i in range(steps):
+    step = 1 if random.randint(0, 1) else -1
+    position += step
+    walk.append(position)
+
+print(walk)
+
+# plt.plot(walk[:100])
+# plt.show()
+print('----------------------------------------')
+
+import numpy as np
+nsteps = 1000
+draws = np.random.randint(0, 2, size=nsteps)
+steps = np.where(draws > 0, 1, -1)
+walk = steps.cumsum()
+print(walk)
+print(walk.min())
+print(walk.max())
+print((np.abs(walk) >= 10).argmax())
+print('-----------------------------------------')
+nwalks = 5000
+nsteps = 1000
+draws = np.random.randint(0, 2, size=(nwalks, nsteps))
+steps = np.where(draws > 0, 1, -1)
+walks = steps.cumsum(1)
+print(walks.max())
+print(walks.min())
+
+hits30 = (np.abs(walks) >= 30).any(1)
+print(hits30)
+print(hits30.sum())
+
+print(hits30)
+print(walks)
+print(walks[hits30])
+
+crossing_times = (np.abs(walks[hits30]) >= 30).argmax(1)
+print(crossing_times)
+print(crossing_times.mean())
+
+
