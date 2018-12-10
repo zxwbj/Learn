e7039ea7209b3305c3beef1e9132aaf43386a810
zxw <1019364135@qq.com>
2018-12-10 15:58:13 +0800

    2018.12.10: learn numpy

---
+++ DataAnalysis/numpy/random_walk_python.py
@@ -0,0 +1,15 @@
+import random
+import matplotlib.pyplot as plt
+
+position = 0
+walk = [position]
+steps = 1000
+for i in range(steps):
+    step = 1 if random.randint(0, 1) else -1
+    position += step
+    walk.append(position)
+
+print(walk)
+
+plt.plot(walk[:100])
+plt.show()
